6faf93561016bdf94b926ef759bb74ed7892c5b7
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-10-19 20:10:31 +0530

    Lecture 12 Time & Space Complexity

---
+++ Lecture 12 Time & Space Complexity/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter N\n";
+    cin >> n;
+    int sum = n + 1 * 2; // Time complexity willbe O(1)
+    cout << sum << "\n";
+    return 0;
+}
