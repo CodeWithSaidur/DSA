8d918214225ca00dcb9132f9ff63b96fa78874f5
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-10-19 06:53:48 +0530

    Lecture 3 Conditional Statements & Loops

---
+++ Lecture 3 Conditional Statements & Loops/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 51;
+    if (a >= 30)
+    {
+        cout << "A grater then 30" << endl;
+    }
+    else
+    {
+        cout << "A = " << a << endl;
+    }
+
+    return 0;
+}
---
+++ Lecture 3 Conditional Statements & Loops/5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 0;
+    int b = 10;
+    do
+    {
+        cout << a << endl;
+        a++;
+    } while (a < b);
+
+    return 0;
+}
